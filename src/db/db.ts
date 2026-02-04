95bbd5c58dd5166c6d4a41497a891832af5d131c
GetParadisePay <getparadisepay@gmail.com>
2026-02-04 20:16:35 +0000

    Update

--- src/db/db.ts
+++ src/db/db.ts
@@ -3,16 +3,20 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const pool = mysql.createPool({
-  host: process.env.MYSQLHOST || process.env.DB_HOST,
-  port: Number(process.env.MYSQLPORT || process.env.DB_PORT || 3306),
-  user: process.env.MYSQLUSER || process.env.DB_USER,
-  password: process.env.MYSQLPASSWORD || process.env.DB_PASSWORD,
-  database: process.env.MYSQLDATABASE || process.env.DB_NAME,
-
-  waitForConnections: true,
-  connectionLimit: 10,
-  namedPlaceholders: true,
-});
+const pool = process.env.MYSQL_PUBLIC_URL
+  ? mysql.createPool(process.env.MYSQL_PUBLIC_URL)
+  : mysql.createPool({
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT ?? 3306),
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+      waitForConnections: true,
+      connectionLimit: 10,
+      namedPlaceholders: true,
+    });
 
 export default pool;
